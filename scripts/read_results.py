157570fe5e24446d006ce4732e96bfeaf4af4fce
universuen <52519513+universuen@users.noreply.github.com>
2022-10-03 17:57:10 +0800

    Update read_results.py

--- scripts/read_results.py
+++ scripts/read_results.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     for ds in results.keys():
         for seed in range(10):
             history = src.History()
-            history.load(config.Paths.results / config.config_name / f'{ds}_tuning_losses_{config.seed}.history')
-            results[ds].append(max(history.values))
+            history.load(config.Paths.results / config.config_name / f'{ds}_tuning_losses_{seed}.history')
+            results[ds].append(history.avg_value)
 
     print(' '.join(results.keys()))
     print(' '.join(map(lambda x: str(sum(x) / len(x)), results.values())))
