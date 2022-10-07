d4a55eef53b8231851f2d6c7979608786da5c32d
universuen <52519513+universuen@users.noreply.github.com>
2022-10-07 11:52:01 +0800

    Update read_results.py

--- scripts/read_results.py
+++ scripts/read_results.py
@@ -21,10 +21,12 @@ if __name__ == '__main__':
             except FileNotFoundError:
                 pass
 
-    def safe_mean(list_):
+
+    def safe_mean(list_) -> int | float:
         return 0 if len(list_) == 0 else sum(list_) / len(list_)
 
-    values = list(map(safe_mean, results.values()))
+
+    values = list(map(lambda x: safe_mean(x) * 100, results.values()))
     values.append(sum(values) / len(values))
     print(' '.join([*[f'{i: <10}' for i in results.keys()], 'mean']))
-    print(' '.join([f'{i: <10.4f}' for i in values]))
+    print(' '.join([f'{i: <10.1f}' for i in values]))
