5d50e9c5e43f3b969b45a81af84b7f61d77b2c0b
universuen <52519513+universuen@users.noreply.github.com>
2022-10-11 16:58:57 +0800

    Update train_prompt_bt.py

--- scripts/train_prompt_bt.py
+++ scripts/train_prompt_bt.py
@@ -1,4 +1,9 @@
 import context
+
+from multiprocessing import Process
+
+import torch
+
 from src import config
 from src import api
 
@@ -6,11 +11,12 @@ DEBUG: bool = False
 CONFIG_NAME: str = 'prompt_bt'
 DEVICE: int = 0
 
-if __name__ == '__main__':
+
+def tune_with_edge_prompt(device: int):
     # set config
-    config.config_name = CONFIG_NAME
+    config.config_name = f'{CONFIG_NAME}_with_edge'
     config.GNN.drop_ratio = 0.5
-    config.device = f'cuda:{DEVICE}'
+    config.device = f'cuda:{device}'
     config.Tuning.use_lr_scheduler = False
     config.Tuning.use_node_prompt = True
     config.Tuning.use_edge_prompt = True
@@ -22,7 +28,12 @@ if __name__ == '__main__':
     """
     gnn_model = api.get_configured_gnn()
     bt_model = api.get_configured_barlow_twins(gnn_model)
-    api.pretrain(bt_model)
+    state_dict = torch.load(
+        config.Paths.models / 'base_bt_model.pt',
+        map_location=lambda storage, loc: storage,
+    )
+    bt_model.load_state_dict(state_dict)
+    bt_model.train()
     """
     Tuning
     """
@@ -33,3 +44,48 @@ if __name__ == '__main__':
             config.TuningDataset.dataset = ds
             api.tune_with_prompt(bt_model.gnn)
     api.analyze_results()
+
+
+def tune_without_edge_prompt(device: int):
+    # set config
+    config.config_name = f'{CONFIG_NAME}_without_edge'
+    config.GNN.drop_ratio = 0.5
+    config.device = f'cuda:{device}'
+    config.Tuning.use_lr_scheduler = False
+    config.Tuning.use_node_prompt = True
+    config.Tuning.use_edge_prompt = False
+    if DEBUG:
+        api.set_debug_mode()
+
+    """
+    Pretraining
+    """
+    gnn_model = api.get_configured_gnn()
+    bt_model = api.get_configured_barlow_twins(gnn_model)
+    state_dict = torch.load(
+        config.Paths.models / 'base_bt_model.pt',
+        map_location=lambda storage, loc: storage,
+    )
+    bt_model.load_state_dict(state_dict)
+    bt_model.train()
+    """
+    Tuning
+    """
+    for seed in config.loop_seeds:
+        config.seed = seed
+        api.set_seed(config.seed)
+        for ds in config.datasets:
+            config.TuningDataset.dataset = ds
+            api.tune_with_prompt(bt_model.gnn)
+    api.analyze_results()
+
+
+if __name__ == '__main__':
+    Process(
+        target=tune_with_edge_prompt,
+        args=(0, ),
+    ).start()
+    Process(
+        target=tune_without_edge_prompt,
+        args=(3,),
+    ).start()
