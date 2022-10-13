9ea0ef552ce36d0ef443278ef5c5fedc4b57a83f
universuen <52519513+universuen@users.noreply.github.com>
2022-10-13 21:31:58 +0800

    Update dual_dataset_v2.py

--- src/dataset/dual_dataset_v2.py
+++ src/dataset/dual_dataset_v2.py
@@ -36,12 +36,18 @@ class DualDatasetV2(Dataset):
             use_original=use_original,
         )
         self.half_len = len(self.other_ds)
+        self.zinc_len = len(self.zinc_ds)
 
     def __len__(self):
         return 2 * self.half_len
 
     def __getitem__(self, item):
-        record = random.choice(self.zinc_ds) if item < self.half_len else self.other_ds[item - self.half_len]
+        if item < self.half_len:
+            record = self.zinc_ds[random.randint(0, self.zinc_len)]
+        elif self.half_len <= item < 2 * self.half_len:
+            record = self.other_ds[item - self.half_len]
+        else:
+            raise IndexError
         b1, b2, other = record
         # delete extra attributes that may cause crash
         delattr(b1, 'fold')
