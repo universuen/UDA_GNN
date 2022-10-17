d214b8ffa1b4e67b76d715ec1025601cf3d1b41b
universuen <52519513+universuen@users.noreply.github.com>
2022-10-17 23:10:26 +0800

    Update train_uda_trans.py

--- scripts/train_uda_trans.py
+++ scripts/train_uda_trans.py
@@ -6,7 +6,7 @@ from src import config
 from src import api
 
 DEBUG: bool = False
-CONFIG_NAME: str = 'uda_trans'
+CONFIG_NAME: str = 'uda_trans_e50_dropN'
 DEVICE: int = 0
 
 if __name__ == '__main__':
