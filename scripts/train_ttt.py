0c4a0d0f149bae5c4166c218a88b510f96b8ed05
universuen <52519513+universuen@users.noreply.github.com>
2022-10-22 14:31:49 +0800

    Update train_ttt.py

--- scripts/train_ttt.py
+++ scripts/train_ttt.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     """
     gnn_model = api.get_configured_gnn()
     bt_model = api.get_configured_barlow_twins(gnn_model)
-    # state_dict = torch.load(
-    #     config.Paths.models / 'graphmae_m35' / 'model_80.pth',
-    #     map_location=lambda storage, loc: storage,
-    # )
+    state_dict = torch.load(
+        config.Paths.models / 'graphmae_m35' / 'model_80.pth',
+        map_location=lambda storage, loc: storage,
+    )
     bt_model.train()
     
     """
@@ -45,6 +45,6 @@ if __name__ == '__main__':
             config.Tuning.use_lr_scheduler = ds == 'bace' 
             config.Tuning.lr = 1e-4 if ds == 'muv' else 1e-3
             bt_model.gnn = api.get_configured_gnn()
-            # bt_model.gnn.load_state_dict(state_dict)
+            bt_model.gnn.load_state_dict(state_dict)
             api.tune_and_save_models(bt_model.gnn)
     api.analyze_ttt_results_by_ratio(item_name='te_auc')
