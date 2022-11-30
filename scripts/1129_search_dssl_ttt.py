939aeb3a5c012717f4806f3e755ece31a4cee311
universuen <52519513+universuen@users.noreply.github.com>
2022-11-30 15:54:25 +0800

    Update 1129_search_dssl_ttt.py

--- scripts/1129_search_dssl_ttt.py
+++ scripts/1129_search_dssl_ttt.py
@@ -77,9 +77,14 @@ def search_para(mask_rate, pretraining_epochs, device):
 
 
 if __name__ == '__main__':
-    for mr in (0.25, 0.40, 0.45):
-        for d, pe in enumerate((10, 20, 50, 100)):
-            Process(
-                target=search_para,
-                args=(mr, pe, d),
-            ).start()
+    # for mr in (0.25, 0.40, 0.45):
+    #     for d, pe in enumerate((10, 20, 50, 100)):
+    #         Process(
+    #             target=search_para,
+    #             args=(mr, pe, d),
+    #         ).start()
+    for d, pe in enumerate((10, 20, 50, 100)):
+        Process(
+            target=search_para,
+            args=(0.35, pe, d),
+        ).start()
