a0f929521e118779d604b50c4a0227eb3ed7bf77
universuen <52519513+universuen@users.noreply.github.com>
2022-11-30 20:31:01 +0800

    Create 1130_presaved_ssf_ttt.py

---
+++ scripts/1130_presaved_ssf_ttt.py
@@ -0,0 +1,55 @@
+import context
+
+from multiprocessing import Process
+from copy import deepcopy
+
+import torch
+
+from src import config
+from src import api
+
+DEBUG: bool = False
+CONFIG_NAME: str = api.get_current_filename(__file__)
+DEVICE: int = 0
+
+if __name__ == '__main__':
+    # set configs
+    config.config_name = CONFIG_NAME
+    config.device = f'cuda:{DEVICE}'
+    config.OneSampleBN.is_enabled = True
+    config.OneSampleBN.strength = 8
+    config.SSF.is_enabled = True
+
+    if DEBUG:
+        api.set_debug_mode()
+    if config.OneSampleBN.is_enabled:
+        api.replace_bn()
+    if config.SSF.is_enabled:
+        api.replace_with_ssf()
+
+    # TTT
+    for seed in config.loop_seeds:
+        config.seed = seed
+        api.set_seed(config.seed)
+        for ds in config.datasets:
+            """
+            TTT
+            """
+            config.TuningDataset.dataset = ds
+            config.TuningLoader.num_workers = 2
+            config.Tuning.batch_size = 32
+            config.TestTimeTuning.aug = 'dropout'
+            config.TestTimeTuning.aug_ratio = 0.5
+            config.TestTimeTuning.num_iterations = 1
+            config.TestTimeTuning.num_augmentations = 32
+            config.Encoder.drop_ratio = 0.5
+            config.Tuning.lr = 1e-3 if ds != 'muv' else 1e-4
+            config.Tuning.use_lr_scheduler = ds == 'bace'
+
+            encoder = api.get_configured_encoder()
+            # encoder.load_state_dict(e_states, strict=False)
+            encoder.enable_selfloop()
+
+            api.test_time_tuning_presaved_models(encoder)
+
+    api.analyze_ttt_results_by_ratio(item_name='te_ttt_auc')
